74e450fb09024dfe1f361b4dffe62805ed8c6f89
Igor Souza <igorsouzaatb@hotmail.com>
2022-07-22 08:49:46 -0300

    Adicionando React-Router-Dom V6

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import {BrowserRouter} from "react-router-dom";
+import {AppRoutes} from './routes'
 
 export const App = ()=> {
   return (
-    <div className="App">
-
-    </div>
+      <BrowserRouter>
+          <AppRoutes/>
+      </BrowserRouter>
   )
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,11 @@
-export {}
+import {Routes, Route, Navigate} from 'react-router-dom'
+
+export const AppRoutes = () =>{
+    return(
+        <Routes>
+            <Route path ='/home' element ={<p>Pagina inicial</p>}>
+            </Route>
+            <Route path='*' element={<Navigate to='/home' />} />
+        </Routes>
+    )
+}
