3db4c7204c6e23211b57c044abdf41af2b86093b
Igor Souza <igorsouzaatb@hotmail.com>
2022-07-25 02:02:09 -0300

    adicionando os atributos dos botoes da barra de detalhes

--- src/shared/components/ferramentas-de-detalhe/FerramentasDeDetalhe.tsx
+++ src/shared/components/ferramentas-de-detalhe/FerramentasDeDetalhe.tsx
@@ -2,56 +2,101 @@ import React from "react";
 import {Box} from "@mui/system";
 import {Button, Divider, Icon, Paper, useTheme} from "@mui/material";
 
+interface IFerramentasDeDetalheProps {
+    textoBotao?: string
 
-export const FerramentasDeDetalhe: React.FC = () => {
-    const theme = useTheme()
+    mostrarBotaoNovo?: boolean
+    mostrarBotaoVoltar?: boolean
+    mostrarBotaoApagar?: boolean
+    mostrarBotaoSalvar?: boolean
+    mostrarBotaoSalvarFechar?: boolean
 
-    return (
-        <Box height={theme.spacing(5)}
-             marginX={1}
-             padding={1}
-             paddingX={1}
-             display='flex'
-             gap={1}
-             alignItems='center'
-             component={Paper}
-        >
-            <Button
-                color='primary'
-                disableElevation
-                variant='contained'
-                startIcon={<Icon>save</Icon>}
-            >Salvar</Button>
-
-            <Button
-                color='primary'
-                disableElevation
-                variant='outlined'
-                startIcon={<Icon>save</Icon>}
-            >Salvar e voltar</Button>
-
-            <Button
-                color='primary'
-                disableElevation
-                variant='outlined'
-                startIcon={<Icon>delete</Icon>}
-            >Apagar</Button>
-
-            <Button
-                color='primary'
-                disableElevation
-                variant='outlined'
-                startIcon={<Icon>add</Icon>}
-            >Novo</Button>
-
-            <Divider variant='middle' orientation='vertical'/>
-
-            <Button
-                color='primary'
-                disableElevation
-                variant='outlined'
-                startIcon={<Icon>arrow_back</Icon>}
-            >Voltar</Button>
-        </Box>
-    )
+    aoClicarEmNovo?: () => void
+    aoClicarEmVoltar?: () => void
+    aoClicarEmApagar?: () => void
+    aoClicarEmSalvar?: () => void
+    aoCLicarEmSalvarFechar?: () => void
 }
+
+export const FerramentasDeDetalhe: React.FC<IFerramentasDeDetalheProps> =
+    ({
+         textoBotao = 'Novo',
+
+         mostrarBotaoSalvarFechar = false,
+         mostrarBotaoApagar = true,
+         mostrarBotaoVoltar = true,
+         mostrarBotaoNovo = true,
+         mostrarBotaoSalvar = true,
+
+         aoClicarEmNovo,
+         aoCLicarEmSalvarFechar,
+         aoClicarEmVoltar,
+         aoClicarEmSalvar,
+         aoClicarEmApagar
+     }) => {
+        const theme = useTheme()
+
+        return (
+            <Box height={theme.spacing(5)}
+                 marginX={1}
+                 padding={1}
+                 paddingX={1}
+                 display='flex'
+                 gap={1}
+                 alignItems='center'
+                 component={Paper}
+            >
+                {mostrarBotaoSalvar && (
+                    <Button
+                        color='primary'
+                        disableElevation
+                        variant='contained'
+                        startIcon={<Icon>save</Icon>}
+                        onClick={aoClicarEmSalvar}
+                    >Salvar</Button>
+                )}
+
+                {mostrarBotaoSalvarFechar && (
+                    <Button
+                        color='primary'
+                        disableElevation
+                        variant='outlined'
+                        startIcon={<Icon>save</Icon>}
+                        onClick={aoCLicarEmSalvarFechar}
+                    >Salvar e voltar</Button>
+                )}
+
+                {mostrarBotaoApagar && (
+                    <Button
+                        color='primary'
+                        disableElevation
+                        variant='outlined'
+                        startIcon={<Icon>delete</Icon>}
+                        onClick={aoClicarEmApagar}
+                    >Apagar</Button>
+                )}
+
+                {mostrarBotaoNovo && (
+                    <Button
+                        color='primary'
+                        disableElevation
+                        variant='outlined'
+                        startIcon={<Icon>add</Icon>}
+                        onClick={aoClicarEmNovo}
+                    >{textoBotao}</Button>
+                )}
+
+                <Divider variant='middle' orientation='vertical'/>
+
+                {mostrarBotaoVoltar && (
+                    <Button
+                        color='primary'
+                        disableElevation
+                        variant='outlined'
+                        startIcon={<Icon>arrow_back</Icon>}
+                        onClick={aoClicarEmVoltar}
+                    >Voltar</Button>
+                )}
+            </Box>
+        )
+    }
